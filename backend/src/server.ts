dc78cdc36794e26a3b76f9582095a7029b5b2708
Developer-Chandan-Dev <chandan.dev.pro@gmail.com>
2025-08-05 10:36:58 +0530

    feat: Origin added in server.ts file (backend)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL ?? "", "http://localhost:5173"],
+    origin: [process.env.CLIENT_URL ?? "", "https://intern-fund.vercel.app", "http://localhost:5173"],
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
     credentials: true,
